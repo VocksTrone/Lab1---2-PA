c332e8efa0f0ed8529074cdb7f0c93d9617d5641
VocksTrone <jr.alvarez0316@gmail.com>
2024-07-30 23:08:27 -0600

    Creación de Método para Mostrar el Menú Principal

--- Lab1 - 2; PA/Program.cs
+++ Lab1 - 2; PA/Program.cs
@@ -2,3 +2,18 @@
 
 List <Products> ProductList = new List<Products>();
 
+
+
+static int ShowMenu()
+{
+    Console.Clear();
+    Console.WriteLine("1. Add Product");
+    Console.WriteLine("2. Consult Information");
+    Console.WriteLine("3. Sell Products");
+    Console.WriteLine("4. Fill Stock");
+    Console.WriteLine("5. Update Price");
+    Console.WriteLine("6. Go Out");
+    Console.WriteLine("\nChoose a Option");
+    int option = int.Parse(Console.ReadLine());
+    return option;
+}
