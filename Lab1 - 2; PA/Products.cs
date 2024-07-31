b9ae6f1f7ff2237c1476e8d06499d6579dbbd68f
VocksTrone <jr.alvarez0316@gmail.com>
2024-07-30 18:47:54 -0600

    Creación de las Funciones y el Constructor de los Productos

--- Lab1 - 2; PA/Products.cs
+++ Lab1 - 2; PA/Products.cs
@@ -8,6 +8,16 @@ namespace Lab1___2__PA
 {
     internal class Products
     {
-
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public Products(string name, string id, double price, int stock)
+        {
+            Name = name;
+            ID = id;
+            Price = price;
+            Stock = stock;
+        }
     }
 }
